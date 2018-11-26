a6bc87f405285d5c382fc7bc7b1022dcca684fdf
thegouge <thegouge42@gmail.com>
2018-11-26 15:27:46 -0800

    added new favicon (with credits) and began styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, {Component} from "react";
 import QuoteBox from "./components/QuoteBox";
+import Footer from "./components/Footer";
 import "./css/App.css";
+
+let background = "rgb(38, 42, 49)";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quoteText: "",
-      quoteAuthor: "",
-      quoteCat: "none"
+      quoteText: "if it ain't broke, break it 'till it's fixed",
+      quoteAuthor: "Brawl Minus",
+      quoteCat: "mahDick"
     };
 
     this.getRandomQuote = this.getRandomQuote.bind(this);
+    this.backgroundChanger = this.backgroundChanger.bind(this);
   }
   getRandomQuote() {
     fetch("https://talaikis.com/api/quotes/random/")
       .then((res) => res.json())
       .then(
         (result) => {
+          this.backgroundChanger(result.cat);
           this.setState({
             isLoaded: true,
             quoteText: result.quote,
@@ -33,12 +38,22 @@ class App extends Component {
         }
       );
   }
+  backgroundChanger(category) {
+    if (this.state.quoteCat !== category) {
+      background = `rgb(${this.getRandomColor}, ${this.getRandomColor}, ${
+        this.getRandomColor
+      })`;
+    }
+  }
+  getRandomColor() {
+    return Math.floor(Math.random() * 256);
+  }
   componentDidMount() {
     this.getRandomQuote();
   }
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{backgroundColor: background}}>
         <header className="App-header">
           <h1>Random Quotes!</h1>
         </header>
@@ -48,6 +63,8 @@ class App extends Component {
           quoteAuthor={this.state.quoteAuthor}
           getQuote={this.getRandomQuote}
         />
+
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      Icons and favicon made by
+      <a href="https://www.freepik.com/" title="Freepik">
+        Freepik
+      </a>
+      from
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+      </a>
+      is licensed by
+      <a
+        href="http://creativecommons.org/licenses/by/3.0/"
+        title="Creative Commons BY 3.0"
+      >
+        CC 3.0 BY
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
