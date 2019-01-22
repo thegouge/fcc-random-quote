310ad74f22554b47b28b3dc4f75d5c1ddbd90c82
thegouge <thegouge42@gmail.com>
2019-01-22 12:24:08 -0800

    updated api call and fine tuned styling

--- src/App.js
+++ src/App.js
@@ -10,46 +10,60 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quoteText: "if it ain't broke, break it 'till it's fixed",
-      quoteAuthor: "Brawl Minus",
-      quoteCat: "mahDick"
+      quotesArray: [],
+      currentQuoteText: "if it ain't broke, break it 'till it's fixed",
+      currentQuoteAuthor: "Brawl Minus"
     };
-
-    this.getRandomQuote = this.getRandomQuote.bind(this);
-    this.backgroundChanger = this.backgroundChanger.bind(this);
   }
-  getRandomQuote() {
-    fetch("https://talaikis.com/api/quotes/random/")
+
+  getQuotesArray = () => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
       .then((res) => res.json())
       .then(
         (result) => {
-          this.backgroundChanger(result.cat);
-          this.setState({
-            quoteText: result.quote,
-            quoteAuthor: result.author,
-            quoteCat: result.cat
-          });
+          this.setState({quotesArray: result.quotes});
+          console.log(result);
         },
         (error) => {
-          this.setState({
-            error
-          });
+          console.log(error);
         }
       );
-  }
-  backgroundChanger(category) {
-    if (this.state.quoteCat !== category) {
-      background = `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`;
+  };
+
+  changeQuote = () => {
+    const newQuote = this.state.quotesArray[this.getRandomNumber(103)];
+    const {currentQuoteText} = this.state;
+    if (currentQuoteText !== newQuote.quote) {
+      this.setState({
+        currentQuoteAuthor: newQuote.author,
+        currentQuoteText: newQuote.quote
+      });
+      this.backgroundChanger(newQuote.author);
+    } else {
+      this.changeQuote();
     }
-  }
-  getRandomColor() {
-    return Math.floor(Math.random() * 256);
-  }
-  componentDidMount() {
-    this.getRandomQuote();
-  }
-  urlText(text) {
+  };
+
+  backgroundChanger = (author) => {
+    if (this.state.currentQuoteAuthor !== author) {
+      background = `rgb(${this.getRandomNumber(256)}, ${this.getRandomNumber(
+        256
+      )}, ${this.getRandomNumber(256)})`;
+    }
+  };
+
+  getRandomNumber = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  urlText = (text) => {
     return text.replace(/\s/, "%20");
+  };
+
+  componentWillMount() {
+    this.getQuotesArray();
   }
   render() {
     return (
@@ -58,12 +72,10 @@ class App extends Component {
           <h1>Random Quotes!</h1>
         </header>
 
-        {this.state.error && <div>{this.state.error}</div>}
-
         <QuoteBox
-          quoteText={this.state.quoteText}
-          quoteAuthor={this.state.quoteAuthor}
-          getQuote={this.getRandomQuote}
+          quoteText={this.state.currentQuoteText}
+          quoteAuthor={this.state.currentQuoteAuthor}
+          getQuote={this.changeQuote}
           quoteIcon={quotes}
           background={background}
           urlText={this.urlText}
