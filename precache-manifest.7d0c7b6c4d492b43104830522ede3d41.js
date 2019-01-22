7607cf66db40e6050712661b43b5333aca2ebdf4
thegouge <thegouge42@gmail.com>
2019-01-22 12:28:45 -0800

    Updates

---
+++ precache-manifest.7d0c7b6c4d492b43104830522ede3d41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dfb472207a355e89120",
+    "url": "/fcc-random-quote/static/js/runtime~main.7dfb4722.js"
+  },
+  {
+    "revision": "ad7c97f414a7fae1bae7",
+    "url": "/fcc-random-quote/static/js/main.ad7c97f4.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/fcc-random-quote/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "ad7c97f414a7fae1bae7",
+    "url": "/fcc-random-quote/static/css/main.1a1b7e40.chunk.css"
+  },
+  {
+    "revision": "d460f804b27d77d574c8797118a3d860",
+    "url": "/fcc-random-quote/index.html"
+  }
+];
