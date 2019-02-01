84fb1e2b8a6bcc3061cd16827499f04760950c99
thegouge <thegouge42@gmail.com>
2019-01-31 19:38:00 -0800

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,13 @@ import "../css/footer.css";
 const Footer = (props) => {
   return (
     <footer style={{backgroundColor: props.background}}>
+      <p>
+        This is a Free Code Camp project made by Alex Gouge-Schajer, using{" "}
+        <a href="https://gist.github.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/">
+          camperbot
+        </a>
+        's random quote library
+      </p>
       Icons and favicon made by
       <a className="footerA" href="https://www.freepik.com/" title="Freepik">
         Freepik
